7f7b6d2367cef1203cd8f07192850d66a2952079
huxley_source <huxley.source@gmail.com>
2013-06-04 18:22:58 +0200

    DataFrame to json dashboard

--- client/app/views.py
+++ client/app/views.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8-*-
 from flask import Flask, render_template
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form, TextField, HiddenField, ValidationError,\
@@ -9,49 +10,26 @@ import json
 
 global_menu_bar = ['dashboard', 'settings']
 data = json.load(open('./app/data/dashboard.json'))
-dash={'headers': ['#', 'sp1', 'sp2', 'sc1', 'sc2', 'sc3', 'sc4'],
- 'rows': [['m5',
-   'Thu 30 May 04:40',
-   'Wed 29 May 20:25',
-   'Thu 30 May 04:40',
-   'Thu 30 May 04:40',
-   'Thu 30 May 18:30',
-   'Wed 29 May 20:25'],
-  ['m10',
-   'Wed 29 May 20:20',
-   'Tue 28 May 10:20',
-   'Tue 28 May 20:50',
-   'Tue 28 May 17:20',
-   'Thu 30 May 15:00',
-   'Thu 30 May 15:00'],
-  ['m15',
-   'Wed 29 May 20:15',
-   'Fri 24 May 13:45',
-   'Tue 28 May 15:30',
-   'Tue 28 May 15:30',
-   'Tue 28 May 20:45',
-   'Tue 28 May 20:45'],
-  ['m30',
-   'Mon 27 May 20:30',
-   'Sat 25 May 12:30',
-   'Fri 24 May 18:30',
-   'Tue 28 May 11:00',
-   'Tue 28 May 15:30',
-   'Mon 29 Apr 13:00'],
-  ['m60',
-   'Fri 24 May 13:00',
-   'Fri 24 May 13:00',
-   'Fri 24 May 18:00',
-   'Mon 27 May 20:00',
-   'Mon 27 May 20:00',
-   '---'],
-  ['m240',
-   'Mon 27 May 20:00',
-   'Mon 27 May 20:00',
-   'Fri 17 May 16:00',
-   'Tue 14 May 20:00',
-   'Fri 24 May 12:00',
-   '---']]}
+dash = {'hheaders': ['m5', 'm10', 'm15', 'm30', 'm60', 'm240'],
+ 'rows': [[{'output': 'Mon 03 Jun 19:40', 'type': 'bear'},
+   {'output': 'Mon 03 Jun 19:40', 'type': 'bear'},
+   {'output': 'Sun 02 Jun 19:30', 'type': 'bear'}],
+  [{'output': 'Mon 03 Jun 19:00', 'type': 'bull'},
+   {'output': 'Mon 03 Jun 19:00', 'type': 'bull'},
+   {'output': 'Sun 02 Jun 20:00', 'type': 'bear'}],
+  [{'output': 'Mon 03 Jun 19:00', 'type': 'bull'},
+   {'output': 'Mon 03 Jun 17:45', 'type': 'bull'},
+   {'output': 'Fri 17 May 16:00', 'type': 'bull'}],
+  [{'output': 'Mon 03 Jun 19:00', 'type': 'bull'},
+   {'output': 'Mon 03 Jun 09:00', 'type': 'bear'},
+   {'output': 'Fri 10 May 17:30', 'type': 'bull'}],
+  [{'output': 'Mon 03 Jun 17:00', 'type': 'bull'},
+   {'output': 'Thu 30 May 21:00', 'type': 'bear'},
+   {'output': '---', 'type': None}],
+  [{'output': 'Sun 02 Jun 08:00', 'type': 'bear'},
+   {'output': 'Sun 02 Jun 08:00', 'type': 'bear'},
+   {'output': '---', 'type': None}]],
+ 'vheaders': ['#', 'wrb', 'wrb_hg', 'fvb']}
 
 
 @app.route('/dashboard')
@@ -63,10 +41,48 @@ def dashboard():
     return render_template('dashboard.html', name=name,
                            global_menu_bar=global_menu_bar,
                            inner_menu=dashboard_menus,
-                           dash=dash)
+                           jdata=dash)
 
 
 @app.route('/settings')
 def settings():
     name = 'settings'
-    return render_template('index.html', name=name, global_menu_bar=global_menu_bar)
+    return render_template('index.html', name=name,
+                           global_menu_bar=global_menu_bar)
+
+
+
+
+
+def jsonify(dfs, columns, names=None, date_format='%a %d %b %H:%M'):
+    if not names:
+        names = ['#']
+        names.extend(columns)
+    else:
+        names.insert(0, '#')
+
+    result = {'vheaders': names, 'hheaders': [], 'rows': []}
+
+    for df in sorted(dfs, key=lambda x: x.index.freq):
+        freq = df.index.freq.delta.total_seconds()/60
+        result['hheaders'].append('m{}'.format(int(freq)))
+        row = []
+        for column in columns:
+            data = {'output': '---', 'type': None}
+            time_ = None
+            try:
+                time_ = df.index[df[column] != 0][-1]
+            except IndexError:
+                pass
+            try:
+                data['output'] = time_.strftime(date_format)
+                data['type'] = 'bull' if df[column][time_] > 0 else 'bear'
+            except (AttributeError, KeyError):
+                pass
+            row.append(data)
+        result['rows'].append(row)
+    return result
+
+
+def json_to_html(jdata):
+    return render_template('table.html', jdata=jdata)
